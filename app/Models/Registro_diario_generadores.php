7dc3be80038e6e7e6de3c9074604cb10c9ed1b25
Fralch <ingfralch@gmail.com>
2022-03-08 11:25:07 -0500

    TIPOS DE DATOS DE LAS TRAMAS

--- app/Models/Registro_diario_generadores.php
+++ app/Models/Registro_diario_generadores.php
@@ -13,39 +13,24 @@ class Registro_diario_generadores extends Model
         'id',
         'contenedor_id',
 
-        'voltage_measured', //Voltage measured show
-        'battery_voltage', //Battery voltage
-        'water_temp', //Water temperature
-        'running_frequency', //Running frequency
-        'fuel_level', //Fuel level
-        'rotor_current', // rotor current 
-        'fiel_current', //Field current
-        'init', // Init
-        'fuel_relay', //Fuel relay
-        'air_heater', //Air heater 
-        'starter', //Starter
-        'restart_20_min', //Restart 20 min
-        'runnig', //Runnig
-        'shutdown', //Shutdown
-        'hardware_error', //Hardware error
-        'start_delay', //Start delay
-        'pti', //PTI
-        'auto_config', //Auto config
-        'stop_by_user', //Stop by user
-        'dealyed_output', //Delayed output
-        'load_measure', //Load measure
-        'delay_voltage', //Delay voltage
-        'speed', //Speed
-        'eco_power', //Eco power
-        'rpm', //RPM
-        'unit_mode', //Unit mode
-
-        'horometro', //Horometro
-        'alarma_id', //Alarma ID
-        'evento_id', //Evento ID
-        'modelo', //Modelo
-        'latitud', //Latitud
-        'longitud', //Longitud
+        'battery_voltage', // Battery Voltage
+        'water_temp', // Water Temperature
+        'running_frequency', // Running Frequency
+        'fuel_level', // Fuel Level
+        'voltage_measured', // Voltage Measured
+        'rotor_current', // Rotor Current
+        'fiel_current', // Field Current
+        'speed', // Speed
+        'eco_power', // Eco Power
+        'rpm', // RPM
+        'unit_mode', // Unit Mode
+        'horometro', // Horometer
+        'alarma_id', // Alarm ID
+        'evento_id', // Event ID
+        'modelo', // Modelo
+        'latitud', // Latitud
+        'longitud', // Longitud
+        'engine_state', // Engine State
         
     ];
 }
