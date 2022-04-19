ce252e0f32259681c265cc1407bc18524c98dc85
Frank Cairampoma <ingfralch@gmail.com>
2022-04-18 20:02:02 -0500

    subiendo a produccion exportarbles

--- resources/js/Pages/Panel/componentes/tabla_detalle_genset.vue
+++ resources/js/Pages/Panel/componentes/tabla_detalle_genset.vue
@@ -1,36 +1,67 @@
 <template >
   <div v-if="tipo == 'genset' && datos_tabla_generador.length > 0">
-    
     <div
       id="generador_grid"
       class="col shadow-sm p-3 mb-5 bg-white rounded"
       style="margin: -30px 15px 10px 15px"
     >
-    <div class="row">
-      <div class="col-3">
-        <div class="input-group input-group-sm mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-sm">Desde</span>
+      <div class="row">
+        <div class="col-3">
+          <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm"
+                >Desde</span
+              >
+            </div>
+            <input
+              type="date"
+              class="form-control"
+              aria-label="Small"
+              aria-describedby="inputGroup-sizing-sm"
+              :value="
+                new Date().getFullYear() +
+                '-' +
+                ('0' + (new Date().getMonth() + 1)).slice(-2) +
+                '-' +
+                ('0' + new Date().getDate()).slice(-2)
+              "
+            />
           </div>
-          <input type="date" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        <div class="col-3">
+          <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Hasta</span>
+            </div>
+            <input
+              type="date"
+              class="form-control"
+              aria-label="Small"
+              aria-describedby="inputGroup-sizing-sm"
+              :value="
+                new Date().getFullYear() +
+                '-' +
+                ('0' + (new Date().getMonth() + 1)).slice(-2) +
+                '-' +
+                ('0' + new Date().getDate()).slice(-2)
+              "
+            />
+          </div>
+        </div>
+        <div class="col">
+          <button
+            id="buscar_detalle"
+            type="button"
+            onclick="alert('Aun estoy trabajando en esto!')"
+          >
+            Buscar
+          </button>
         </div>
       </div>
-      <div class="col-3">
-         <div class="input-group input-group-sm mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-sm">Hasta</span>
-          </div>
-          <input type="date" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-        </div>
-      </div>
-      <div class="col">
-         <button id="buscar_detalle_genset" type="button" onclick="alert('Hello world!')">Buscar</button>
-      </div>
-    </div>
-      
+
       <table
         class="display nowrap"
-        style="width:100%"
+        style="width: 100%"
         id="tblDetalleContenedores_generadores"
       >
         <thead>
@@ -136,21 +167,19 @@ export default {
           scrollX: "100%",
           responsive: true,
           buttons: true,
-          dom: 'Bfrtip',
+          dom: "Bfrtip",
           buttons: [
-              // 'copy', 'csv', 'excel', 'pdf','print'
-               'copy', 'csv', 'excel'
+            // 'copy', 'csv', 'excel', 'pdf','print'
+            "copy",
+            "csv",
+            "excel",
           ],
           order: [0, "desc"],
           language: {
             url: "//cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json",
           },
         });
-
-        
       });
-      
-     
     },
     select_contenedor() {
       let self = this;
@@ -178,12 +207,14 @@ export default {
               tipo: "genset",
             })
             .then((response) => {
-            
               if (response.data.length > 0) {
-                self.ubicacion = [response.data[0].latitud,response.data[0].longitud];
-              }else{
+                self.ubicacion = [
+                  response.data[0].latitud,
+                  response.data[0].longitud,
+                ];
+              } else {
                 self.ubicacion = null;
-              }              
+              }
             })
             .then(() => {
               self.$emit("set_ubicacion_g", self.ubicacion);
@@ -195,41 +226,4 @@ export default {
 };
 </script>
 <style>
-#buscar_detalle_genset {
-  min-width: 190px;
-  height: 30px;
-  color: #fff;
-  padding: 2px 10px;
-  font-weight: bold;
-  cursor: pointer;
-  transition: all 0.3s ease;
-  position: relative;
-  display: inline-block;
-  outline: none;
-  border-radius: 5px;
-  border: none;
-  box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);
-  background: #17A2B8;
- z-index: 1;
-}
-#buscar_detalle_genset:hover:after {
-  width: 100%;
-  left: 0;
-}
-#buscar_detalle_genset:after {
-  border-radius: 5px;
-  position: absolute;
-  content: "";
-  width: 0;
-  height: 100%;
-  top: 0;
-  z-index: -1;
-  box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5), 7px 7px 20px 0px rgba(0,0,0,.1), 4px 4px 5px 0px rgba(0,0,0,.1);
-  transition: all 0.3s ease;
-  background-color: #096574;
-  right: 0;
-}
-.css-button-shadow-border-sliding--blue:active {
-  top: 2px;
-}
 </style>
