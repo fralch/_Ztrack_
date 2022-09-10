13d8e783973d60fcf6f4492b76d116d608cba930
Frank <ingfralch@gmail.com>
2022-09-10 11:43:27 -0500

    responsive done

--- resources/js/Pages/layout.vue
+++ resources/js/Pages/layout.vue
@@ -1,13 +1,16 @@
 <template>
   <div style="height: 100% !important">
     <div class="container-fluid">
-        <nav class="col-12 navbar navbar-expand-lg navbar-light bg-light" style="height:40px !important; background-color: #0E5976 !important; box-shadow: 0px 0px 13px 2px rgba(0,0,0,0.15) !important;">
+        <nav class="col-12 navbar navbar-expand-lg navbar-light bg-light" style="height:40px ; background-color: #0E5976 !important; box-shadow: 0px 0px 13px 2px rgba(0,0,0,0.15) !important;">
           <img :src="'/images/logos/copos-de-nieve.png'" alt="Logo" width="20px" /> 
           <!-- <img src="http://162.248.55.24/resources/images/zgroup_logo.png" alt="Logo" width="30px" />  -->
           &nbsp;
           <a class="navbar-brand" href="/board" style="font-family: Poppins-Regular; text-transform: uppercase;color: white;">Zgroup</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button id="opcionesnav" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
+          </button>
+          <button id="logoutnav" style="display:none; color: white; " class="navbar-toggler" type="button" @click="cerrarSesion()" >
+            <span class="bi bi-x"></span>
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -22,7 +25,7 @@
                 <a class="nav-link disabled" href="#">Disabled</a>
               </li> -->
             </ul>
-            <form class="form-inline my-2 my-lg-0">
+            <form class="form-inline my-2 my-lg-0" id="fromnav">
              <ul class="navbar-nav mr-auto">
               <li class="nav-item dropdown">
                 <a  class="nav-link dropdown-toggle" 
@@ -127,6 +130,13 @@ export default {
     nav{
      width: 100% !important;
      height: 60px !important;
+    }
+    nav #opcionesnav{
+       display: none !important;
+    }
+    
+    nav #logoutnav{
+       display: block !important;
     }
   }
 
